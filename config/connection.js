bd3b2c70cd8d9843ae8e99c454e086e36b442e5b
Edinh <edinhurem@gmail.com>
2022-04-17 13:09:48 -0400

    conn changes

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,14 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
